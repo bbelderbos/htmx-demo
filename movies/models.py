08aee6da6b3cd5ffe4b498c3516003921ff67c1e
Bob Belderbos <bobbelderbos@gmail.com>
2023-07-18 15:04:49 +0200

    add app and model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
