25d2d1ac1890d0cdca7df53a3cf23d52d559fffe
Bob Belderbos <bobbelderbos@gmail.com>
2023-07-18 15:18:02 +0200

    make title unique

--- movies/models.py
+++ movies/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, unique=True)
     genre = models.CharField(max_length=100)
 
     def __str__(self):
