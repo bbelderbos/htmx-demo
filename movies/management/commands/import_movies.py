d723253db24b6b7d11c382b156d86e146db6f4ec
Bob Belderbos <bobbelderbos@gmail.com>
2023-07-18 15:20:46 +0200

    fix import script

--- movies/management/commands/import_movies.py
+++ movies/management/commands/import_movies.py
@@ -1,6 +1,8 @@
 import csv
 from django.core.management.base import BaseCommand
 from movies.models import Movie
+from django.db.utils import IntegrityError
+
 
 class Command(BaseCommand):
     help = 'Imports movies from a CSV file'
@@ -10,15 +12,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         csv_file = options['csv_file']
-        i = 0
+        insert_count = 0
         with open(csv_file, 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
-                i += 1
                 title, genres = row
                 main_genre = genres.split('|')[0]
-                movie = Movie.objects.create(title=title, genre=main_genre)
-                self.stdout.write(self.style.SUCCESS(f'Successfully imported movie: {movie}'))
-        self.stdout.write(self.style.SUCCESS(f'{i} movies imported'))
+                try:
+                    movie = Movie.objects.create(title=title, genre=main_genre)
+                except IntegrityError:
+                    self.stderr.write(self.style.ERROR(f'Skip, {movie} is already in the database'))
+                    continue
+                else:
+                    insert_count += 1
+                    # self.stdout.write(self.style.SUCCESS(f'Successfully imported movie: {movie}'))
+        self.stdout.write(self.style.SUCCESS(f'{insert_count} movies imported'))
 
