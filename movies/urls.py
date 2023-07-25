5a39674ed3c90429cd13d5f38df28a8b04db7d50
Bob Belderbos <bobbelderbos@gmail.com>
2023-07-25 18:01:09 +0200

    base feature done

--- movies/urls.py
+++ movies/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('movies', views.get_more_movies, name='get_more_movies'),
     path('search', views.search_movies, name='search_movies'),
+    path('<int:pk>', views.show_movie, name='show_movie'),
     path('<int:pk>/delete', views.delete_movie, name='delete_movie'),
+    path('<int:pk>/edit', views.edit_movie, name='edit_movie'),
 ]
